84a138b4c44e44d1ab4c92fa1175b188dca0c3a0
Latika Mishra <92726604+latikamishra28@users.noreply.github.com>
2024-03-13 15:29:18 +0530

    First  commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 4444;
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+// console.log(app);
+
+// middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+app.use("/", require("./routes/blog"));
+hbs.registerPartials(__dirname + '/views/partials');
+
+mongoose.connect('mongodb://127.0.0.1:27017/BlogPage')
+.then(()=>{
+    console.log('Database connected successfully');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+
+
+// for sending the hbs file
+// app.get('/getblog', (req, res)=>{
+//     res.render('blogpage', {
+//         firstname: "Arihant",
+//         middlename:"Vinayak",
+//         lastname:"Shukla"
+//     });
+// });
